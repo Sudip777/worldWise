984baac19ca49e83d0a2c35594a1fec5187086f1
Sudip Parajuli <sudipparajuli06@gmail.com>
2023-08-06 23:07:55 +0545

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import AppNav from './AppNav'
+import Logo from './Logo'
+import Footer from './Footer'
+import styles from './Sidebar.module.css'
+import { Outlet } from 'react-router-dom'
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+        <Logo/>
+        <AppNav/>
+        <Outlet/>
+        <Footer/>
+
+      
+    </div>
+  )
+}
